06d0e4a2abe10c7e1f1bb131ebf28851d03bba6a
Simon Mennig <simon.mennig@hotmail.de>
2019-01-09 20:35:13 +0100

    Integrate Bluetooth again

--- app/src/main/java/com/example/android/sonar/MainActivity.java
+++ app/src/main/java/com/example/android/sonar/MainActivity.java
@@ -1,52 +1,183 @@
 package com.example.android.sonar;
 
-import android.app.Activity;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.View;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
-public class MainActivity extends Activity {
+import com.example.android.sonar.ble.BlunoLibrary;
+
+
+public class MainActivity extends BlunoLibrary {
+    public static final int BAUD = 115200;
+    private static final String TAG = "MainActivity";
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+    private Button buttonScan;
+
+    private SonarGLSurfaceView sonarGLSurfaceView;
+
     private SonarRenderer renderer;
+
+    private int angle;
+    private int distance;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
-        LinearLayout ll = findViewById(R.id.main_layout);
+        this.setContentView(R.layout.activity_sonar);
+        LinearLayout ll = findViewById(R.id.fullscreen_content_controls);
+        this.angle = 0;
+        this.distance = 0;
+
+        this.onCreateProcess();                                                        //onCreate Process by BlunoLibrary
+
+        this.getNeededPermissions();
+        this.serialBegin(BAUD);                                                    //set the Uart Baudrate on BLE chip to 115200
+
+        this.sonarGLSurfaceView = findViewById(R.id.glvSonarView);
+        this.buttonScan = this.findViewById(R.id.ConnectButton);                    //initial the button for scanning the BLE device
+        this.buttonScan.setOnClickListener(v -> {
+            buttonScanOnClickProcess();                                        //Alert Dialog for selecting the BLE device
+        });
+
         this.renderer = new SonarRenderer();
-        SonarGLSurfaceView glView = new SonarGLSurfaceView(this, this.renderer);
+        this.sonarGLSurfaceView.init(this.renderer);
+        this.renderer.setAngle(angle);
+        this.renderer.setDistance(distance);
+
         Button btn = new Button(this);
         btn.setText("raise");
         btn.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        BtnOnClickListener clickListener = new BtnOnClickListener(this.renderer);
-        btn.setOnClickListener(clickListener);
-        ll.addView(btn);
-        ll.addView(glView);
+        btn.setOnClickListener(c -> {
+            this.renderer.setAngle(++this.angle);
+            this.renderer.setDistance(this.angle % 2);
 
-        this.renderer.setAngle(0);
-        this.renderer.setDistance(0);
+
+            System.out.println(angle);
+        });
+        ll.addView(btn);
+
+
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "coarse location permission granted");
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle(getString(R.string.constrained_fuctionality));
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(dialog -> {
+                    });
+                    builder.show();
+                }
+            }
+        }
+    }
+
+
     protected void onResume() {
         super.onResume();
+        System.out.println("BlUNOActivity onResume");
+        this.onResumeProcess();                                                        //onResume Process by BlunoLibrary
     }
 
-    private class BtnOnClickListener implements View.OnClickListener {
-        private SonarRenderer renderer;
-        private int angle;
 
-        public BtnOnClickListener(SonarRenderer renderer)
-        {
-            this.renderer = renderer;
-        }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        this.onActivityResultProcess(requestCode, resultCode, data);                    //onActivityResult Process by BlunoLibrary
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
-        @Override
-        public void onClick(View v) {
-            this.renderer.setAngle(++this.angle);
-            this.renderer.setDistance(this.angle % 2);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.onPauseProcess();                                                        //onPause Process by BlunoLibrary
+    }
+
+    protected void onStop() {
+        super.onStop();
+        this.onStopProcess();                                                        //onStop Process by BlunoLibrary
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.onDestroyProcess();                                                        //onDestroy Process by BlunoLibrary
+    }
+
+    @Override
+    public void onConectionStateChange(connectionStateEnum theConnectionState) {//Once connection state changes, this function will be called
+        switch (theConnectionState) {                                            //Four connection state
+            case isConnected:
+                this.buttonScan.setText(getString(R.string.connected));
+                break;
+            case isConnecting:
+                this.buttonScan.setText(getString(R.string.connecting));
+                break;
+            case isToScan:
+                this.buttonScan.setText(getString(R.string.scan));
+                break;
+            case isScanning:
+                this.buttonScan.setText(getString(R.string.scanning));
+                break;
+            case isDisconnecting:
+                this.buttonScan.setText(getString(R.string.isDisconnecting));
+                break;
+            default:
+                break;
         }
     }
+
+
+    @Override
+    public void onSerialReceived(int distance, int angle) {                            //Once connection data received, this function will be called
+        this.angle = angle;
+        this.distance = distance;
+
+        this.renderer.setAngle(angle);
+        this.renderer.setDistance(distance);
+    }
+
+
+    public int getAngle() {
+        return angle;
+    }
+
+    public void setAngle(int angle) {
+        this.angle = angle;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    private void getNeededPermissions() {
+        if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(getString(R.string.loction_permission_required));
+            builder.setMessage(getString(R.string.location_permission_required_message));
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setOnDismissListener(dialog -> MainActivity.this.requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION));
+            builder.show();
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/android/sonar/SonarGLSurfaceView.java
+++ app/src/main/java/com/example/android/sonar/SonarGLSurfaceView.java
@@ -3,14 +3,15 @@ package com.example.android.sonar;
 import android.content.Context;
 import android.graphics.PixelFormat;
 import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
 
 public class SonarGLSurfaceView extends GLSurfaceView {
-    public SonarGLSurfaceView(Context context, GLSurfaceView.Renderer renderer) {
-        super(context);
-        init(renderer);
+
+    public SonarGLSurfaceView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
     }
 
-    private void init(GLSurfaceView.Renderer renderer) {
+    public void init(GLSurfaceView.Renderer renderer) {
         this.getHolder().setFormat(PixelFormat.TRANSLUCENT);
         setEGLContextClientVersion(2);
         setRenderer(renderer);
